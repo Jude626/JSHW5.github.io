d9cfdf7575b30ae46bc29544d82bda97f200f83a
Jude <jantungkuno@gmail.com>
2020-12-10 14:48:42 -0500

    Added an array & ids for the time being used (9-5)

--- script.js
+++ script.js
@@ -8,5 +8,8 @@ var timeDiv = $("#currentDay");
 timeDiv.append(timeDisplay);
 
 // Make an array for the times to be displayed on the left of the planner //
+var timeList = ["9 AM", "10 AM", "11 AM", "12 AM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
 // Make a string to give an id for each time //
+var idTime = ["9", "10", "11", "12","13", "14",  "15",  "16", "17"];
+
